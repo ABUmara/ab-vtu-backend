47c3630a59fe246f9e3904b5d31d052a56ecaf87
ABUmara <abdullahialhumara07@gmail.com>
2026-07-03 22:33:10 +0100

    initial backend

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('AB VTU Backend is running!');
+});
+
+// Test API Route
+app.post('/api/test', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Backend is working!',
+    data: req.body
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
